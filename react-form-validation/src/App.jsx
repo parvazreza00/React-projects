a7354a7c640ef8a592e13674d00fd50e2cd97161
parvazreza00 <parvazreza00@gmail.com>
2026-05-13 10:24:03 +0600

    react-form-validation

--- react-form-validation/src/App.jsx
+++ react-form-validation/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 
 import './App.css'
 import Form from './Components/From-validation/Form'
+import Form_2 from './Components/From-validation/Form_2'
 
 function App() {
 
   return (
    <div>
-    <Form />
+    {/* <Form /> */}
+    <Form_2/>
    </div>
   )
 }
