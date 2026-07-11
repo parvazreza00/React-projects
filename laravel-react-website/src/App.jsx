cee9fa0625da8f58d05958fda900629d83a06214
parvazreza00 <parvazreza00@gmail.com>
2026-07-11 09:31:14 +0600

    larave reavt  webiste

---
+++ laravel-react-website/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import TopBanner from './components/topBanner/TopBanner'
+
+
+function App() {
+
+
+  return (
+   <div>
+
+    <TopBanner/>
+    
+
+   </div>
+  )
+}
+
+export default App
---
+++ laravel-react-website/src/components/topBanner/TopBanner.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import "../../assets/css/custom.css";
+
+export class TopBanner extends Component {
+  render() {
+    return <div className="topFixBanner p-0">
+        <div className="topBannerOverlay">
+            <Container className="topContent">
+                <Row>
+                    <Col className="text-center">
+                    <h1 className="topTitle">React Laravel Website</h1>
+                    <h4 className="topSubTitle">Learning with project</h4>
+                    <Button variant="primary">Learn More</Button>
+                    </Col>
+                </Row>
+            </Container>
+
+        </div>
+
+    </div>;
+  }
+}
+
+export default TopBanner;
