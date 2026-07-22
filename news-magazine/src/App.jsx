9af7956a23a4ae93aa47ced428cd2b928ce67aef
parvazreza00 <parvazreza00@gmail.com>
2026-07-22 09:58:41 +0600

    update

--- news-magazine/src/App.jsx
+++ news-magazine/src/App.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Newsboard from "./components/Newsboard";
 
 function App() {
+  const [category, setCategory] = useState('general')
 
   return (
     <div>
-     <Navbar/>
-     <Newsboard/>
+     <Navbar setCategory={setCategory}/>
+     <Newsboard category={category}/>
     </div>
   );
 }
