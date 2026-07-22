71e057eb5698cac300cffbad228e2a7867a65b3c
parvazreza00 <parvazreza00@gmail.com>
2026-07-22 18:23:15 +0600

    porps idea

--- react19-laravel/src/App.jsx
+++ react19-laravel/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import {UserCard} from "./components/feature/UserCard";
 import Header from "./components/layout/Header";
 
 function App() {
   return (
     <>
-      <Header />
-      <h1>Hello React19-Laravel</h1>
+      {/* <Header />
+      <h1>Hello React19-Laravel</h1> */}
+      <UserCard name="Programming Feilds" technology="Reactjs"/>
+      <UserCard />
     </>
   );
 }
---
+++ react19-laravel/src/components/feature/UserCard.jsx
@@ -0,0 +1,12 @@
+export const UserCard = ({name="Parvaz", technology="Laravel"}) => {
+
+  return (
+    <div>
+        <h1>User Name : {name}</h1>
+        <p>Technology : {technology}</p>
+      
+    </div>
+  )
+}
+
+// export default UserCard
